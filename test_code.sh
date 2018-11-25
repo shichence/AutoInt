77a60e97ce3084d6af39b953f63da2025e102c48
Chence Shi <chenceshi@pku.edu.cn>
2018-11-25 16:23:18 +0800

    Update test_code.sh

--- test_code.sh
+++ test_code.sh
@@ -2,7 +2,7 @@ echo "start training ..."
 
 python -u train.py \
                        --data "Criteo"  --blocks 3 --heads 2  --block_shape "[64, 64, 64]" \
-                       --is_save "False" --save_path "./test_code/Criteo/b3h2_64x64x64/"  \
+                       --is_save "True" --save_path "./test_code/Criteo/b3h2_64x64x64/"  \
                        --field_size 39  --run_times 1 --data_path "./" \
                        --epoch 3 --has_residual "True"  --has_wide "False" \
                        > test_code_single.out &
@@ -10,7 +10,7 @@ python -u train.py \
 
 python -u train.py \
                        --data "Criteo" --blocks 3 --heads 2  --block_shape "[64, 64, 64]" \
-                       --is_save "False" --save_path "./test_code/Criteo/b3h2_dnn_dropkeep1_400x2/"  \
+                       --is_save "True" --save_path "./test_code/Criteo/b3h2_dnn_dropkeep1_400x2/"  \
                        --field_size 39  --run_times 1 --dropout_keep_prob "[1, 1, 1]" --data_path "./" \
                        --epoch 3 --has_residual "True"  --has_wide "False"  --deep_layers "[400, 400]"\
                        > ./test_code_dnn.out &
